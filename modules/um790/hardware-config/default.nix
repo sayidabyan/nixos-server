b7881cc93a9f046858b09da63417f7338d9ec093
Sayid Abyan Rizal Shiddiq <sayidabyan@gmail.com>
2024-01-13 02:42:55 +0000

    separate swapfile config

--- modules/um790/hardware-config/default.nix
+++ modules/um790/hardware-config/default.nix
@@ -23,11 +23,6 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ {
-    device = "/var/lib/swapfile";
-      size = 16*1024;
-  } ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
