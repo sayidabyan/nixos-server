b0562eed98020d333200b430b1ae323fefc08342
Sayid Abyan Rizal Shiddiq <sayidabyan@gmail.com>
2024-01-15 07:39:02 +0000

    add conda

--- modules/common/python/default.nix
+++ modules/common/python/default.nix
@@ -3,7 +3,8 @@
 {  
     home-manager.users.sayid = {pkgs, ...}: {
         home.packages = with pkgs; [
-            python312
+            conda
+	    python312
             python312Packages.pip
         ];
     };
