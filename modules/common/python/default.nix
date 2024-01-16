decb84413c96f876c8c8e6bee1906233bf80825a
Sayid Abyan Rizal Shiddiq <sayidabyan@gmail.com>
2024-01-16 06:46:44 +0000

    change to python311

--- modules/common/python/default.nix
+++ modules/common/python/default.nix
@@ -4,8 +4,9 @@
     home-manager.users.sayid = {pkgs, ...}: {
         home.packages = with pkgs; [
             conda
-	    python312
-            python312Packages.pip
+	    python311
+            python311Packages.pip
+	    python311Packages.python-lsp-server
         ];
     };
 }
