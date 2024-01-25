9f8000952d3802b1609a0024ccb83f411080ce73
sayid <sayidabyan@gmail.com>
2024-01-25 07:39:49 +0000

    add xps13 ssh pubkey and configure tlp

--- modules/common/ssh/default.nix
+++ modules/common/ssh/default.nix
@@ -10,5 +10,6 @@
     users.users.sayid.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINqFi6txBF5FtkV10/LKMsxUK82PMbFpEYZJF2g7fYCP sayid@Sayids-MacBook-Pro.local"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO/PyHcXklsFeMvkESVHQfeO6NO3kDfoBF3wKkAjdgiH sayidabyan@gmail.com"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPEDdTC2vkW+j8YOj9p/U7O9kTnpMVzPqCfKw/iHKrX6 sayid@nixos"
     ];
 }
