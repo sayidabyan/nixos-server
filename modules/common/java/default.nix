7c7cfc9caf3eae57f91c1181cd3abd3cdd03fd77
Sayid Abyan Rizal Shiddiq <sayidabyan@gmail.com>
2024-01-28 14:28:43 +0000

    remove conflicting jre

--- modules/common/java/default.nix
+++ modules/common/java/default.nix
@@ -4,8 +4,8 @@
     home-manager.users.sayid = {pkgs, ...}: {
         home.packages = with pkgs; [
             temurin-bin-11
-	    temurin-jre-bin-11
 	    maven
+	    eclipses.eclipse-java
         ];
     };
 }
