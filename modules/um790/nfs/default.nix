10c9612cdb7ca9fe62e6d7fff518cf875655f6f4
Sayid Abyan Rizal Shiddiq <sayidabyan@gmail.com>
2024-01-31 00:02:42 +0000

    update nfs

--- modules/um790/nfs/default.nix
+++ modules/um790/nfs/default.nix
@@ -2,6 +2,6 @@
 {
     services.nfs.server.enable = true;
     services.nfs.server.exports = ''
-        /etc/share 100.72.80.85 (rw,fsid=0,no_subtree_check)
+    /etc/share  100.72.80.85(rw,nohide,insecure,no_subtree_check)
     '';
 }
