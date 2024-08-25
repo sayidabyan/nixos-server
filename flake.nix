1f4df4154011fb5f9341d4273c9b2d757df3acc1
Sayid Abyan Rizal Shiddiq <2948284s@student.gla.ac.uk>
2024-08-25 01:59:02 +0100

    reformat

--- flake.nix
+++ flake.nix
@@ -7,9 +7,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:nixos/nixos-hardware/master";
-    nixpkgs.url  = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
     nixos-cosmic = {
       url = "github:lilyinstarlight/nixos-cosmic";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -21,86 +20,91 @@
       url = "github:aiken-lang/editor-integration-nvim";
       flake = false;
     };
-  };
-  outputs = inputs:
-  let
-    buildSystem = builtins.mapAttrs (device: modules:
-    let
-      nixpkgs = inputs.nixpkgs;
-      home-manager = inputs.home-manager;
-      nixos-cosmic = inputs.nixos-cosmic;
-      lib = nixpkgs.lib;
-      modulesInDir = dir: (lib.trivial.pipe dir [
-        builtins.readDir
-        (lib.attrsets.filterAttrs (key: val: val == "directory"))
-        builtins.attrNames
-        (builtins.map (name: dir + "/${name}"))
-      ]);
-    in
-      nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit inputs;
-          inherit device;
-          username = "sayid";
-        };
-      modules =
-        modulesInDir ./modules/common ++
-        modulesInDir (./modules + "/${device}") ++
-        [
-          ({ pkgs, ... }: {
-            nix = {
-                registry.nixpkgs.flake = nixpkgs;
-                settings.experimental-features = [ "nix-command" "flakes" ];
-            };
-          # Required for nixos-rebuild with flakes
-            environment.systemPackages = [ pkgs.git ];
-          })
-          {
-            nix.settings = {
-              substituters = [ "https://cosmic.cachix.org/" ];
-              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-            };
-          }
-          nixos-cosmic.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-
-          # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
-          nix.registry.nixpkgs.flake = inputs.nixpkgs;
-
-          nixpkgs.overlays = [
-            (final: prev: {
-              unstable = import inputs.nixpkgs-unstable {
-                system = final.system;
-                config.allowUnfree = true;
-              };
-              vimPlugins =
-                let
-                  vimPlugin = name: final.vimUtils.buildVimPlugin {
-                    name = name;
-                    src = inputs.${name};
-                  };
-                in
-                prev.vimPlugins // {
-                  editor-integration-nvim = vimPlugin "editor-integration-nvim";
-                };
-            })
-          ];
-        }
-        ] ++ modules;
-      }
-    );
-  in
-  {
-    nixosConfigurations = buildSystem {
-      um790 = [ 
-        inputs.nixos-hardware.nixosModules.common-gpu-amd
-        inputs.nixos-hardware.nixosModules.common-cpu-amd
-        inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
-      ];
-      xps13 = [
-        inputs.nixos-hardware.nixosModules.dell-xps-13-9370
-      ];
+    winresizer-vim = {
+      url = "github:simeji/winresizer";
+      flake = false;
     };
   };
+  outputs = inputs:
+    let
+      buildSystem = builtins.mapAttrs (device: modules:
+        let
+          nixpkgs = inputs.nixpkgs;
+          home-manager = inputs.home-manager;
+          nixos-cosmic = inputs.nixos-cosmic;
+          lib = nixpkgs.lib;
+          modulesInDir = dir: (lib.trivial.pipe dir [
+            builtins.readDir
+            (lib.attrsets.filterAttrs (key: val: val == "directory"))
+            builtins.attrNames
+            (builtins.map (name: dir + "/${name}"))
+          ]);
+        in
+        nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs;
+            inherit device;
+            username = "sayid";
+          };
+          modules =
+            modulesInDir ./modules/common ++
+            modulesInDir (./modules + "/${device}") ++
+            [
+              ({ pkgs, ... }: {
+                nix = {
+                  registry.nixpkgs.flake = nixpkgs;
+                  settings.experimental-features = [ "nix-command" "flakes" ];
+                };
+                # Required for nixos-rebuild with flakes
+                environment.systemPackages = [ pkgs.git ];
+              })
+              {
+                nix.settings = {
+                  substituters = [ "https://cosmic.cachix.org/" ];
+                  trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+                };
+              }
+              nixos-cosmic.nixosModules.default
+              home-manager.nixosModules.home-manager
+              {
+
+                # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
+                nix.registry.nixpkgs.flake = inputs.nixpkgs;
+
+                nixpkgs.overlays = [
+                  (final: prev: {
+                    unstable = import inputs.nixpkgs-unstable {
+                      system = final.system;
+                      config.allowUnfree = true;
+                    };
+                    vimPlugins =
+                      let
+                        vimPlugin = name: final.vimUtils.buildVimPlugin {
+                          name = name;
+                          src = inputs.${name};
+                        };
+                      in
+                      prev.vimPlugins // {
+                        editor-integration-nvim = vimPlugin "editor-integration-nvim";
+                        winresizer-vim = vimPlugin "winresizer-vim";
+                      };
+                  })
+                ];
+              }
+            ] ++ modules;
+        }
+      );
+    in
+    {
+      nixosConfigurations = buildSystem {
+        um790 = [
+          inputs.nixos-hardware.nixosModules.common-gpu-amd
+          inputs.nixos-hardware.nixosModules.common-cpu-amd
+          inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
+        ];
+        xps13 = [
+          inputs.nixos-hardware.nixosModules.dell-xps-13-9370
+        ];
+      };
+    };
 }
