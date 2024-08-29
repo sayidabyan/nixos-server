8e1eabfe094ad67b35b057235422a6a8d92fc9fe
Sayid Abyan Rizal Shiddiq <2948284s@student.gla.ac.uk>
2024-08-29 05:47:56 +0100

    hyprland new window take over fullscreen

--- modules/common/hyprland/default.nix
+++ modules/common/hyprland/default.nix
@@ -392,6 +392,7 @@
           disable_hyprland_logo = true;
           disable_splash_rendering = true;
           mouse_move_enables_dpms = true;
+          new_window_takes_over_fullscreen = 1;
         };
          
         dwindle = {
