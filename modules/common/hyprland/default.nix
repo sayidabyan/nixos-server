20ae38dae7d651a513d2ec3d7e36805bae64655e
sayidabyan <sayidabyan@proton.me>
2024-11-18 04:11:54 +0700

    comment drop shadow

--- modules/common/hyprland/default.nix
+++ modules/common/hyprland/default.nix
@@ -356,7 +356,7 @@
         decoration = {
           rounding = "10";
 
-          drop_shadow = "false";
+          # drop_shadow = "false";
           # shadow_range = "4";
           # shadow_render_power = "3";
           # "col.shadow" = "rgba(1a1a1aee)";
