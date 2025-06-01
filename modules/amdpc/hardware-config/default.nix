2a9b6711b3ef710ef43f62c4a3d4830e943b3014
sayid <sayidabyan@gmail.com>
2025-06-01 19:16:29 +0700

    1 june #2

--- modules/amdpc/hardware-config/default.nix
+++ modules/amdpc/hardware-config/default.nix
@@ -12,7 +12,7 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
-  boot.kernelPackages = pkgs.linuxPackages_6_14;
+  boot.kernelPackages = pkgs.unstable.linuxPackages_latest;
   
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/e80f686b-0184-4390-8d31-b7debc5be1fe";
