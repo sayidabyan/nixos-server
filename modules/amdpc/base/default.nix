99923cce80124201411cdf5418202afa2867d7aa
sayid <sayidabyan@gmail.com>
2025-07-27 09:31:39 +0700

    ollama backend + opencl for mesa-git

--- modules/amdpc/base/default.nix
+++ modules/amdpc/base/default.nix
@@ -18,9 +18,12 @@
     enable = true;
     enable32Bit = true;
     package = pkgs.unstable.mesa;
-    extraPackages = with pkgs; [rocmPackages.clr.icd];
+    extraPackages = with pkgs; [unstable.rocmPackages.clr.icd];
   };
-  chaotic.mesa-git.enable = true;
+  chaotic.mesa-git = {
+    enable = true;
+    extraPackages = with pkgs; [unstable.rocmPackages.clr.icd];
+  };
   boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
   
@@ -49,4 +52,24 @@
     # PS5 DualSense controller over USB hidraw
     ATTRS{name}=="Sony Interactive Entertainment DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
   '';
+
+  services.ollama = {
+    enable = true;
+    package = pkgs.unstable.ollama;
+    acceleration = "rocm";
+    host = "0.0.0.0";
+  };
+  services.nginx.virtualHosts."ollama.say.id" = {
+    locations."/" = {
+      proxyPass = "http://100.72.251.119:11434";
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+      extraConfig = ''
+        client_max_body_size 10000M;
+        proxy_read_timeout   600s;
+        proxy_send_timeout   600s;
+        send_timeout         600s;
+      '';
+    };
+  };
 }
